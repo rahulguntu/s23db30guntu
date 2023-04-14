d11bb462a4f39cd8a11edabb0744c5b7355192fa
rahulguntu <guntu.ece@gmail.com>
2023-04-14 11:11:21 -0500

    modified a files

--- controllers/costume.js
+++ controllers/costume.js
@@ -7,7 +7,10 @@ res.send('NOT IMPLEMENTED: Costume list');
 exports.costume_detail = function(req, res) {
 res.send('NOT IMPLEMENTED: Costume detail: ' + req.params.id);
 };
-
+// Handle Costume create on POST.
+exports.costume_create_post = function(req, res) {
+res.send('NOT IMPLEMENTED: Costume create POST');
+};
 // Handle Costume delete form on DELETE.
 exports.costume_delete = function(req, res) {
 res.send('NOT IMPLEMENTED: Costume delete DELETE ' + req.params.id);
@@ -62,4 +65,34 @@ exports.costume_create_post = async function(req, res) {
     res.send(`{"error": ${err}}`);
     }
     };
-    
+    // for a specific Costume.
+exports.costume_detail = async function(req, res) {
+    console.log("detail" + req.params.id)
+    try {
+    result = await Costume.findById( req.params.id)
+    res.send(result)
+    } catch (error) {
+    res.status(500)
+    res.send(`{"error": document for id ${req.params.id} not found`);
+    }
+    };
+
+exports.costume_update_put = async function(req, res) {
+console.log(`update on id ${req.params.id} with body
+${JSON.stringify(req.body)}`)
+try {
+let toUpdate = await Costume.findById( req.params.id)
+// Do updates of properties
+if(req.body.costume_type)
+toUpdate.costume_type = req.body.costume_type;
+if(req.body.cost) toUpdate.cost = req.body.cost;
+if(req.body.size) toUpdate.size = req.body.size;
+let result = await toUpdate.save();
+console.log("Sucess " + result)
+res.send(result)
+} catch (err) {
+res.status(500)
+res.send(`{"error": ${err}: Update for id ${req.params.id}
+failed`);
+}
+};
