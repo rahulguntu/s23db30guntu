a3b1f4f166a0782157dd1777c3ffe6f098b689f7
rahulguntu <guntu.ece@gmail.com>
2023-04-20 20:27:48 -0500

    modified a files

--- app.js
+++ app.js
@@ -3,6 +3,26 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+  Account.findOne({ username: username }, function (err, user) {
+  if (err) { return done(err); }
+  if (!user) {
+  return done(null, false, { message: 'Incorrect username.' });
+  }
+  if (!user.validPassword(password)) {
+  return done(null, false, { message: 'Incorrect password.' });
+  }
+  return done(null, user);
+  });
+  }));
+  
+   var Account =require(('./models/account'));
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -23,6 +43,13 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(require('express-session')({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false
+ }));
+ app.use(passport.initialize());
+ app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
@@ -72,7 +99,7 @@ async function recreateDB(){
   Costume({costume_type:"ghost", size:'large',
   cost:15.4});
   let instance2 = new
-  Costume({costume_type:"Nikki", size:'tingari',
+  Costume({costume_type:"Nikki", size:'Tingari',
   cost:15.4});
   let instance3 = new
   Costume({costume_type:"Pinky", size:'Fonky',
